d5fb0eeae6d7caeabecbfa83be00bcfe53057bba
LIANGCA3 <carol.t.t.liang@oocl.com>
2019-03-09 12:11:07 +0800

    finish entity

--- src/main/java/com/oocl/ita/demo/entites/Account.java
+++ src/main/java/com/oocl/ita/demo/entites/Account.java
@@ -1,8 +1,10 @@
 package com.oocl.ita.demo.entites;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "account")
@@ -16,7 +18,61 @@ public class Account {
     private User user;
     private Type type;
     private Double account;
-    private Boolean
+    private Boolean accountKind;
+    private Date date;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Double getAccount() {
+        return account;
+    }
+
+    public void setAccount(Double account) {
+        this.account = account;
+    }
+
+    @Column(name = "account_kind")
+    public Boolean getAccountKind() {
+        return accountKind;
+    }
+
+    public void setAccountKind(Boolean accountKind) {
+        this.accountKind = accountKind;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
--- src/main/java/com/oocl/ita/demo/entites/Type.java
+++ src/main/java/com/oocl/ita/demo/entites/Type.java
@@ -1,4 +1,55 @@
 package com.oocl.ita.demo.entites;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "type")
 public class Type {
+
+    public Type(){
+
+    }
+    private Integer id;
+    private String type;
+    private Boolean accountKind;
+    private List<Account> accountList = new ArrayList<>();
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Column(name = "account_kind")
+    public Boolean getAccountKind() {
+        return accountKind;
+    }
+
+    public void setAccountKind(Boolean accountKind) {
+        this.accountKind = accountKind;
+    }
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "account", fetch = FetchType.LAZY)
+    public List<Account> getAccountList() {
+        return accountList;
+    }
+
+    public void setAccountList(List<Account> accountList) {
+        this.accountList = accountList;
+    }
 }
