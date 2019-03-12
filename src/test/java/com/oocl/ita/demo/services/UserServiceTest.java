806489617b58317e4650505495fd3946ed0dd053
lzjjj <1358999379@qq.com>
2019-03-12 22:42:57 +0800

    [louis] init userServiceTest.

---
+++ src/test/java/com/oocl/ita/demo/services/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.oocl.ita.demo.services;
+
+import mockit.integration.junit4.JMockit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+@RunWith(JMockit.class)
+public class UserServiceTest {
+}
